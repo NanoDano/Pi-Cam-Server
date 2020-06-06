3ac1fbb694a173525973015a510a74f923f6d68d
John Leon <nanodano@devdungeon.com>
2020-06-06 18:59:16 -0500

    AutoPushUpdate

--- app.py
+++ app.py
@@ -33,7 +33,9 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html', context={'hostname': gethostname()})
+        hostname = gethostname()
+        print(f'hostname: {hostname}')
+        return render_template('index.html', context={'hostname': hostname})
     now = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
     with PiCamera() as camera:
         camera.annotate_background = Color('green')
